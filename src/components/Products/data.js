9f6b6df5d38f56fa03dc186c9c2824e53f11375c
Silas Silva <silvasilas66@gmail.com>
2021-06-26 18:10:38 -0300

    Creating Products component

---
+++ src/components/Products/data.js
@@ -0,0 +1,60 @@
+import product1 from '../../images/product-1.jpg';
+import product2 from '../../images/product-2.jpg';
+import product3 from '../../images/product-3.jpg';
+import sweet from '../../images/sweet.jpg';
+import sweet2 from '../../images/sweet-3.jpg';
+import sweet3 from '../../images/sweet3.jpg';
+
+export const productData = [
+  {
+    img: product1,
+    alt: 'Pizza',
+    name: 'Pizza suprema',
+    desc: 'Molho marinara, manjericão, salsicha italiana, tomate roma, azeitonas e molho pesto',
+    price: 'R$40,99',
+    button: 'Adicionar ao carrinho'
+  },
+  {
+    img: product2,
+    alt: 'Pizza',
+    name: 'Paraíso havaiano',
+    desc: 'Molho marinara, manjericão, salsicha italiana, tomate roma, azeitonas e molho pesto',
+    price: 'R$52,99',
+    button: 'Adicionar ao carrinho'
+  },
+  {
+    img: product3,
+    alt: 'Pizza',
+    name: 'Sobrecarga vegetal',
+    desc: 'Molho marinara, manjericão, salsicha italiana, tomate roma, azeitonas e molho pesto',
+    price: 'R$89,99',
+    button: 'Adicionar ao carrinho'
+  }
+];
+
+export const productDataTwo = [
+    {
+        img: sweet,
+        alt: 'Donuts',
+        name: 'Donuts dos céus',
+        desc: 'Donuts com cobertura de chocolate belga, glacê e granulado para finalizar',
+        price: 'R$15,00',
+        button: 'Add to Cart'
+    },
+    {
+        img: sweet2,
+        alt: 'Sorvete',
+        name: 'Maravilha de caramelo',
+        desc: 'Sorvete de baunilha com cobertura de caramelo e uma cama de bolo de chocolate, além de um palito de coco',
+        price: 'R$7,99',
+        button: 'Adicionar ao carrinho'
+    },
+    {
+        img: sweet3,
+        alt: 'Brownie',
+        name: 'Montalha de brownies',
+        desc: 'Brownies cobertos com pedaços de chocolate branco e nozes de macadâmia',
+        price: 'R$10,50',
+        button: 'Adicionar ao carrinho'
+    }
+];
